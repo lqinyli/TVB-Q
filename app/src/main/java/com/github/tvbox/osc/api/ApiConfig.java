f456a8ffda6c5e7d4cd24db43a7b710bb56a4570
lqinyli <lqinyli@139.com>
2022-08-26 09:01:05 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -80,7 +80,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "http://88d.cn/mtv");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
